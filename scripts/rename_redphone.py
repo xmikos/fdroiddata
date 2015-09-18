88dd1b714137ae60693b20b1c46cd1804632160f
Michal Krenek (Mikos) <m.krenek@gmail.com>
2015-09-18 21:49:31 +0200

    TextSecure renamed to TextLibre, RedPhone renamed to PhoneLibre, repo updated

---
+++ scripts/rename_redphone.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import os, sys, re
+from pathlib import Path
+
+
+oldname = "RedPhone"
+newname = "PhoneLibre"
+re_string = re.compile(r'(?s)(<string.*?>)(.*?)(</string>)')
+
+
+def string_repl(match):
+    return ''.join([
+        match.group(1),
+        match.group(2).replace(oldname, newname),
+        match.group(3)
+    ])
+
+
+def update_strings(f):
+    oldfile = open(f).read()
+    newfile = re_string.sub(string_repl, oldfile)
+    with open(f, "w") as fd:
+        fd.write(newfile)
+
+
+def main():
+    basepath = sys.argv[1]
+
+    # Rename strings
+    for p in Path(basepath).glob('**/strings.xml'):
+        f = str(p)
+        print(f)
+        update_strings(f)
+
+
+if __name__ == '__main__':
+    main()
