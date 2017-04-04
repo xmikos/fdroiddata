c6f72178a81734bdc075cc3a3e15ca7f05f0400d
Michal Krenek (Mikos) <m.krenek@gmail.com>
2017-04-04 11:58:51 +0200

    New Signal version; switch to Signal "website" build flavor; change app update URL

--- scripts/rename_signal.py
+++ scripts/rename_signal.py
@@ -9,6 +9,9 @@ renames = [('Signal', 'LibreSignal'),
            ('RedPhone', 'PhoneLibre')]
 re_string = re.compile(r'(?s)(<string.*?>)(.*?)(</string>)')
 
+replacement_url = 'https://fdroid.eutopia.cz/updates/libresignal.json'
+re_url = re.compile(r'(ext[.]websiteUpdateUrl\s*=\s*")(http[s]?://.+?)(")')
+
 
 def string_repl(match):
     rename_group = match.group(2)
@@ -29,14 +32,26 @@ def update_strings(f):
         fd.write(newfile)
 
 
+def update_url(f):
+    oldfile = open(f).read()
+    newfile = re_url.sub(r'\1{}\3'.format(replacement_url), oldfile)
+    with open(f, "w") as fd:
+        fd.write(newfile)
+
+
 def main():
     basepath = sys.argv[1]
 
-    # Rename strings
+    print('Renaming strings:')
     for p in Path(basepath).glob('**/strings.xml'):
         f = str(p)
-        print(f)
+        print('  {}'.format(f))
         update_strings(f)
+
+    print('Replacing update URL:')
+    f = str(Path(basepath) / 'build.gradle')
+    print('  {}'.format(f))
+    update_url(f)
 
 
 if __name__ == '__main__':
